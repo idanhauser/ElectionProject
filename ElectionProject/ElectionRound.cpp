3e1996925d8e6ff3225ed59f42aadce415fdde6b
Roeeht <49073465+Roeeht@users.noreply.github.com>
2020-12-11 16:38:00 +0200

    Reuslts func updated

--- ElectionProject/ElectionRound.cpp
+++ ElectionProject/ElectionRound.cpp
@@ -63,45 +63,51 @@ namespace elec
 	}
 
 	void ElectionRound::theResults(resultsArr& votesResults) {
-	
 
-/*******************************************************************************************/
+
+		/*******************************************************************************************/
 		District* tempDis;
 		resultsArr countReps;
 		char* pmWithTheMostRepsName;
-		Citizen *partyLeader;
-		int totalDistrictVotes = 0, partyLeaderReps, maxRepsForPm=0, totalPartyVotesPrecentage, partyVotesInDistrict;
+		Citizen* partyLeader;
+		int totalDistrictVotes = 0, partyLeaderReps, maxRepsForPm = 0, totalPartyVotesPrecentage, partyVotesInDistrict, totalPartyVotes = 0;
 		int districtAmount = countReps.getdistrictsAmount(), partiesAmount = countReps.getpartiesAmount();
-		
+
 		for (int j = 0; j <= districtAmount; j++) {
 			tempDis = _districts.findDistrictByID(j + 100);
 			cout << "\n----------------------------------------------------";
+			//לכל מחוז יש לההציג את שם המחוז
 			cout << "\nDistrict Name: " << tempDis->getName();
+			//מספר הנציגים שהמחוז מעניק
 			cout << "\nAmount Of Reps: " << tempDis->getNumOfReps();
 			for (int k = 0; k < partiesAmount; k++) {
 				partyVotesInDistrict = votesResults.getDistrictNumberOfVotesInParty(j + 100, k);
 				totalPartyVotesPrecentage = partyVotesInDistrict / tempDis->getNumberOfCitizens() * 100;//from all citizens - not only voting ones
-				cout << "\nThe party " << _parties.findPartyByID(k) << " got " << partyVotesInDistrict << " votes"<< //delete after fix of the next comment
+				cout << "\nThe party " << _parties.findPartyByID(k) << " got " << partyVotesInDistrict << " votes" << //delete after fix of the next comment
 					"\nwhich are " << totalPartyVotesPrecentage << "precentage of votes from all citizens";
-				
+
 				/*todo: allow these lines after adding "getVotingCitizensAmountInDistrict()" func:
-				
+				//מספר ואחוז הקולות שקיבלה כל מפלגה
 				totalPartyVotesPrecentage = partyVotesInDistrict / tempDis->getVotingCitizensAmountInDistrict() * 100;
-				cout << "\nThe party " << _parties.findPartyByID(k) << " got " << partyVotesInDistrict << " votes"<< 
+				cout << "\nThe party " << _parties.findPartyByID(k) << " got " << partyVotesInDistrict << " votes"<<
 					"\nwhich are " << partyVotesInDistrict/getVotingCitizensAmountInDistrict()*100 <<
 					"precentages of voting citizens in the district";
-				
+
 				*/
-				
+
 				partyLeaderReps = partyVotesInDistrict / tempDis->getNumberOfCitizens() * (tempDis->getNumOfReps());
+				//update: explanation: setElectionResult member in party k district j.
+				//  implementation:                    
+				//_parties.findPartyByID(k).findDistrictByID(j).setElectionResult(partyLeaderReps)
 				
 				/*todo: allow these lines after adding "printPartyRepsFromDistrictByAmount()" func:
-				
-				printPartyRepsFromDistrictByAmount(partyLeaderReps)
+				//להציג לכל מחוז את רשימת הנציגים שנבחרה מכל מפלגה
+				findPartyByID(k).printPartyRepsFromDistrictByAmount(partyLeaderReps, districtID)
+
 				partyLeader = _parties.findPartyByID(k).getPartyLeader();
 				cout << "\nThe Party leader " << partyLeader.getCitizenName() << " got " <<
 					partyLeaderReps << "Reps";
-					
+
 					*/
 
 				if (partyLeaderReps > maxRepsForPm) {
@@ -109,49 +115,73 @@ namespace elec
 					strcpy(pmWithTheMostRepsName, partyLeader->getCitizenName());
 				}
 			}
-			
-			/*todo: allow these lines after adding "getVotingCitizensAmountInDistrict()" func:
 
+			/*todo: allow these lines after adding "getVotingCitizensAmountInDistrict()" func:
+			//אחוז ההצבעה במחוז
 			cout << "The precentage of voting citizens in the District is: " <<
 				tempDis.getVotingCitizensAmountInDistrict()/tempDis->getEligibleCitizens() * 100;
-				
+
 				*/
 
 
-			//district winner check:
-
+				//district winner check:
+				//מועמד המפלגה אליה המחוז משויך
 
 			cout << "the Pm party with the most reps is: " << partyLeader->getCitizenName() << " who get all " <<
 				tempDis->getNumOfReps() << " reps ";
-			
+
 			countReps.setPmsRepsByPartyID(partyLeader->getParty()->getPartyID());
 			maxRepsForPm = 0;
 			/*todo: allow this line after adding "setLeader(Citizen)" func:
 
 			tempDis.setLeader(partyLeader);
-			
+
 			*/
 		}
 		
-		// elections winner check:
+		/***************************************************************************************/
+		//pmCandidatesRepsArrSorted[] sort:
+		struct pair {
+			int index;
+			int repsAmount;
+		};
+		pair* pmCandidatesRepsArrSorted = new pair[partiesAmount];
+
+		for (int m = 0; m < partiesAmount; m++) {
+			pmCandidatesRepsArrSorted[m].index = m;
+			pmCandidatesRepsArrSorted[m].repsAmount = countReps.getPmsRepsByPartyID(m);
+		}
+		int n = sizeof(pmCandidatesRepsArrSorted) / sizeof(int);
+		int min;
+		pair temp;
+		for (int i = 0; i < n - 1; i++) {
+			min = i;
+			for (int j = i + 1; j < n; j++)
+				if (pmCandidatesRepsArrSorted[j].repsAmount < pmCandidatesRepsArrSorted[min].repsAmount)
+					min = j;
+			temp = pmCandidatesRepsArrSorted[i];
+			pmCandidatesRepsArrSorted[i] = pmCandidatesRepsArrSorted[min];
+			pmCandidatesRepsArrSorted[min] = temp;
+		}
+		/***************************************************************************************/
 		
-		/* todo: allow these lines after =>
-		//=>fix  _parties.findPartyByID(i).PartyLeader() to get in some other working way the citizen partyLeader
+		for (int i = 0; i < partiesAmount; i++) {
+	//להציג עבור כל מפלגה את המועמד שלה לראשות המדינה, סכום הנציגים שזכה בהם וסכום הקולות שקיבלה המפלגה בכל המחוזות
+			for (int l = 0; l < partiesAmount; l++)
+				totalPartyVotes = totalPartyVotes + votesResults.getDistrictNumberOfVotesInParty(l + 100, i);
+			cout << _citizens.findCitizenByID(_parties.findPartyByID(pmCandidatesRepsArrSorted[i].index)->getPartyPMCandidateID())->getCitizenName() << //todo: fix finding partyLeader ID
+				" got total amount of " << countReps.getPmsRepsByPartyID(pmCandidatesRepsArrSorted[i].index) << " reps " <<
+				"\nhis party got total amount of" << totalPartyVotes << " votes";
+		}
+		cout << "the ELECTIONS WINNER is: " << _citizens.findCitizenByID(_parties.findPartyByID(pmCandidatesRepsArrSorted[0].index)->getPartyPMCandidateID())->getCitizenName()
+			<< "with" << countReps.getPmsRepsByPartyID(pmCandidatesRepsArrSorted[0].repsAmount) << "electors";
+
+		delete[] pmCandidatesRepsArrSorted;
+
 		
-		
-		for (int i = 0; i < partiesAmount; i++)
-			if (maxRepsForPm < countReps.getPmsRepsByPartyID(i)) {
-				//Citizen winner = _parties.findPartyByID(i).PartyLeader(); 
-				maxRepsForPm = countReps.getPmsRepsByPartyID(i);
-			}
-		cout << "the ELECTIONS WINNER is: " << winner->getCitizenName() << "with" <<
-			countReps.getPmsRepsByPartyID(winner->getParty().getPartyID()) << "electors";
+	}
 
-
-		*/
-
-
-
+	
 
 
 
